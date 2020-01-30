f5ef22f924fc0815613edb90f7c639be54e320c1
sharang kulkarni <45118333+sharangKulkarni2@users.noreply.github.com>
2020-01-29 21:52:08 -0800

    Update binary_search.py

--- binary_search.py
+++ binary_search.py
@@ -9,5 +9,20 @@ def binary_search(arr, start, end, key):
         return binary_search(arr, mid + 1, end, key)
     else:
         return binary_search(arr, start, mid - 1, key)
-       
+    
+def binary_search_iterative(arr, start, end, key):
+    while(start <= end):
+        mid = (start + end) / 2
+        mid = int(mid)
+        if arr[mid] == key:
+            return mid
+        elif arr[mid] < key:
+            start = mid + 1
+        else:
+            end = mid - 1
+            
+            
 print(binary_search([x for x in range(100)], 0, 99, 7))
+    
+       
+
